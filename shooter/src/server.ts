bfd1515b3640264130c3d55c70cb6ca386874e15
Pia <algauer.henrique@gmail.com>
2024-12-25 22:38:20 -0300

    third opt

--- shooter/src/server.ts
+++ shooter/src/server.ts
@@ -1,9 +1,10 @@
-import ws from "ws";
-import { createGameRunner } from "./game";
+import { createGameRunner, onClose, onMessage } from "./game";
 import * as consts from "./game/consts";
 import { getConfig } from "./cli";
 import { getLogger, initLogger } from "./logger";
 import { getWriter } from "./game/data-writer";
+
+import * as uws from "uWebSockets.js"
 
 const args = getConfig();
 consts.initFromEnv();
@@ -11,28 +12,33 @@ consts.initFromCLI(args);
 initLogger(args);
 getWriter(args);
 
-const server = new ws.Server({ port: args.port });
 const runner = createGameRunner();
 
 getLogger().info(args, "starting server");
 
-let id = 0;
-server.on("connection", (socket) => {
-    getLogger().info("new connection");
-    // @ts-ignore
-    socket.MY_ID = ++id;
+uws.App().ws('/*', {
 
-    runner(socket);
-});
+})
 
+/* One app per thread; spawn as many as you have CPU-cores and let uWS share the listening port */
+uws.App().ws('/*', {
+    close: (ws) => {
+        onClose(ws)
+    },
+    open: (ws) => {
+        runner(ws)
+    },
+    message: (ws, message) => {
+        onMessage(ws, Buffer.from(message).toString())
+    }
 
-server.on("listening", () => {
-    getLogger().info("listening on", args.port);
-    console.log("listening on", args.port);
-});
-
-server.on("error", (err) => {
-    getLogger().error({ err }, "cannot start server");
-    console.error("cannot start server", err);
-});
+}).listen(args.port, (listenSocket) => {
+    if (listenSocket) {
+        getLogger().info("listening on", args.port)
+        console.log("listenig on", args.port)
+    } else {
+        getLogger().error("cannot start server")
+        console.log("cannot start server")
+    }
+})
 
